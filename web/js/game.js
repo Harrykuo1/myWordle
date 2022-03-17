35fc6fa090a944a15ac2015c99eb1cd30ace4b98
Harrykuo1 <stu970157@gmail.com>
2022-03-17 18:56:19 +0800

    Change color & create seeting page

--- web/js/game.js
+++ web/js/game.js
@@ -19,55 +19,68 @@ $(document).keydown(function (e) {
     else {
         alert("Valid Key")
     }
-});  
+});
 
 eel.expose(vendorBoard)
-function vendorBoard(ymax, xmax){
+function vendorBoard(ymax, xmax) {
     let gameBoard = document.getElementById("gameBoard");
-    for(let j=0; j<ymax; j++){
+    for (let j = 0; j < ymax; j++) {
         let rowDiv = document.createElement("div");
         rowDiv.setAttribute("class", "row d-flex gap-3");
         rowDiv.setAttribute("id", "boardRow" + j);
-        
-        for(let i=0; i<xmax; i++){
+
+        for (let i = 0; i < xmax; i++) {
             let colDiv = document.createElement("div");
             colDiv.setAttribute("class", "textBox");
-            colDiv.setAttribute("id", "no" + (j*xmax + i));
+            colDiv.setAttribute("id", "no" + (j * xmax + i));
             rowDiv.appendChild(colDiv)
 
             let span = document.createElement("span");
             span.setAttribute("class", "fs3");
-            span.setAttribute("id", "noWord" + (j*xmax + i));
+            span.setAttribute("id", "noWord" + (j * xmax + i));
             colDiv.appendChild(span)
         }
         gameBoard.appendChild(rowDiv);
     }
 }
 eel.expose(modifyBoard)
-function modifyBoard(y, x, ymax, xmax, key){
-    let pos = y*xmax + x
+function modifyBoard(y, x, ymax, xmax, key) {
+    let pos = y * xmax + x
     let item = document.getElementById("noWord" + pos)
     item.innerText = key
 }
 
 eel.expose(drawColor)
-function drawColor(y , xmax,colorList){
-    let posBasic = y*xmax
-    for(let i=0; i<xmax;i++){
+function drawColor(y, xmax, colorList) {
+    let posBasic = y * xmax
+    for (let i = 0; i < xmax; i++) {
         let block = document.getElementById("no" + (posBasic + i));
-        if(colorList[i] == 0){
-            block.classList.add("bg-secondary")
+        if (colorList[i] == 0) {
+            block.classList.add("myGrayBackground")
         }
-        else if(colorList[i] ==1 ){
-            block.classList.add("bg-warning")
+        else if (colorList[i] == 1) {
+            block.classList.add("myYellowBackground")
         }
-        else{
-            block.classList.add("bg-success")
+        else {
+            block.classList.add("myGreenBackground")
         }
     }
 }
 
 eel.expose(webAlert)
-function webAlert(str){
+function webAlert(str) {
     alert(str)
 }
+
+/*
+function start() {
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+    var timer = document.querySelector("#timer");
+    var number = 10;
+    setInterval(function () {
+        number--;
+        if (number <= 0)
+            number = 0;
+        timer.innerText = number + 0
+    }, 1000);
+}*/
